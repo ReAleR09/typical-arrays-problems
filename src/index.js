ab968a5c2621bc7406cf8600d7d551932cef9ac6
Bogdan Z <realer09@gmail.com>
2020-03-17 21:41:51 +0300

    feat: implement solution for "typical-arrays-problems" task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+    if( !(array instanceof Array) || array.length === 0) {
+        return 0;
+    }
+    let min = Number.MAX_SAFE_INTEGER;
+    for(let i in array) {
+        if(array[i] < min) {
+            min = array[i];
+        }
+    }
+
+     return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if( !(array instanceof Array) || array.length === 0) {
+        return 0;
+    }
+    let max = Number.MIN_SAFE_INTEGER;
+    for(let i in array) {
+        if(array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if( !(array instanceof Array) || array.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    for(let i in array) {
+        sum += array[i];
+    }
+
+    return (sum / array.length);
 }
